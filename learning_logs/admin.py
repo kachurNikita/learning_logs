12d395150e7c5a7099b27aef5dc9a1c947530bad
Nikita <ashtonashtonkachur@gmail.com>
2023-02-01 21:42:07 -0500

    Second commit, not informative, because add not to much

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from learning_logs.models import Topic
+from learning_logs.models import Entry
 
 
 # Register your models here.
 # Importing topic model, then tell Django to manage our model trough admin site
 admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -25,6 +25,8 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        return self.text[:50]
-
+        if len(self.text) >= 50:
+            return self.text[:50]
+        else:
+            return self.text
 
